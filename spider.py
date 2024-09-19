5c03ea1a686cbf92fd49b09455bff47faa698632
oraz <oraz.ospanov98@gmail.com>
2024-09-20 01:11:08 +0500

    fix bug

--- spider.py
+++ spider.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import random
 import time
 from datetime import datetime
 
@@ -12,9 +13,9 @@ class ProxySubmissionSpider(scrapy.Spider):
     start_urls = ['http://free-proxy.cz/en/']
 
     personal_token = "t_d222c9ef"
-    final_proxies={}
+    final_proxies=[]
     custom_settings = {
-        "DOWNLOAD_DELAY": 5
+        "DOWNLOAD_DELAY": 1
     }
 
     def start_requests(self):
@@ -41,23 +42,34 @@ class ProxySubmissionSpider(scrapy.Spider):
             if proxy_ip and proxy_port:
                 proxies.append(f"{proxy_ip}:{proxy_port}")
         print(proxies)
-        # Once proxies are scraped, submit the form
-        proxy_chunks = [proxies[i:i+10] for i in range(0, len(proxies), 10)]
+        self.final_proxies.extend(proxies)
+
+    def closed(self, reason):
+        # define final json for proxies.json file
+        proxies_json = {}
+        proxy_chunks = [self.final_proxies[i:i + 10] for i in range(0, len(self.final_proxies), 10)]
         for ten_proxies_arr in proxy_chunks:
             form_data = {'user_id': 't_d222c9ef',
                          'len': len(ten_proxies_arr),
                          'proxies': ', '.join(ten_proxies_arr)
                          }
             resp1 = requests.get('https://test-rg8.ddns.net/api/get_token')
-            time.sleep(2)
             print(resp1.cookies.get('form_token'))
-            resp2 = requests.post(url='https://test-rg8.ddns.net/api/post_proxies',
-                                  json=form_data,
-                                  headers={'cookie':f"user_id={self.personal_token};form_token={resp1.cookies.get('form_token')}"})
-            saved_id = resp2.json()['save_id']
-            self.final_proxies[saved_id] = proxies
-
-    def closed(self, reason):
+            while True:
+                resp2 = requests.post(url='https://test-rg8.ddns.net/api/post_proxies',
+                                      json=form_data,
+                                      headers={
+                                          'cookie': f"user_id={self.personal_token};form_token={resp1.cookies.get('form_token')}"},
+                                      proxies={})
+                if not resp2.ok:
+                    print(resp2.text)
+                    print(resp2)
+                else:
+                    saved_id = resp2.json()['save_id']
+                    proxies_json[saved_id] = ten_proxies_arr
+                    break
+                time.sleep(2+random.uniform(1,2))
+            time.sleep(20 + random.uniform(0, 1))
         end_time = datetime.now()
         elapsed_time = end_time - self.start_time
         hours, remainder = divmod(elapsed_time.seconds, 3600)
@@ -65,4 +77,4 @@ class ProxySubmissionSpider(scrapy.Spider):
         with open('time.txt', 'w') as f:
             f.write(f"{hours:02}:{minutes:02}:{seconds:02}")
         with open('proxies.json', 'w') as f:
-            json.dump(self.final_proxies, f)
+            json.dump(proxies_json, f)
